9f0b063cd3410333247ff33beb72565c371448a7
marman2 <mario.mancuso@hotmail.it>
2024-09-21 17:40:05 +0200

    endpoint

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -23,7 +23,7 @@ const ChatComponent: React.FC = () => {
     setLoading(true);
     try {
       const response = await axios.post<QueryResponse>(
-        'https://3146-35-189-177-217.ngrok-free.app/query',
+        'https://d675-35-189-177-217.ngrok-free.app/query',
         { question }
       );
       setChatHistory([...chatHistory, { question, response: response.data }]);
